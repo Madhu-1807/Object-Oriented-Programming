7618f89700ba5a485e53a6ff606dfb9fa4dbeb4d
Madhu-1807 <senthilkumarmadhu1981@gmail.com>
2025-09-17 09:33:51 +0530

    Lecture-06 done

---
+++ Lecture-06/01.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+class temple_run{
+    private:
+        int score;
+    public:
+        void left(void);
+        void right(void);
+        void get_score(void);
+        temple_run(){
+            this->score = 0;
+            cout<<"Game started with score :"<<score<<endl;
+        }
+        // friend function
+        friend void bonus(temple_run* t);
+
+};
+void temple_run ::left(void){
+    this->score ++;
+}
+void temple_run ::right(void){
+    this->score ++;
+}
+void temple_run:: get_score(void){
+    cout<<" Your score is :"<< this->score<<endl;
+}
+// void bonus(class temple_run p){
+// }
+void bonus(temple_run* p6){
+    // p6.score = p6.score + 5;
+    p6->score = p6->score + 5;
+
+}
+int main(){
+    temple_run p1;
+    p1.left();
+    p1.right();
+    p1.get_score();
+    bonus(&p1); // normal function
+    p1.get_score();
+
+    return 0;
+  
+}
---
+++ Lecture-06/02.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int * arr = (int*)malloc(sizeof(int)*5);
+    int n;
+    cout<<"Enter the size of an array"<<endl;
+    cin>>n;
+    int * arr = new int[n];// dynamically allocated
+    arr[0]= 5; 
+    arr[1] = 4;
+    arr[2] = 3;
+    cout<<" "<<arr[0]<<" "<<arr[1]<<" "<<arr[2]<<endl;
+    // free(arr);
+    delete[] arr; // clearing memory
+    return 0;
+}
